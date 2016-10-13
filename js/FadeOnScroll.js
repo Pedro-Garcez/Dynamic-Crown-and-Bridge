ee271a730c07a6cb8933c667ea098f24b931d3ee
Pedro Garcez <pedropaulogarcez@gmail.com>
2016-10-12 22:56:55 -0400

    Improved the script. Added animations for every section.

--- js/FadeOnScroll.js
+++
@@ -1,3 +0,0 @@
-$(window).scroll(function(){
-$(".fade-custom").css("opacity", 1 - $(window).scrollTop() / 400);
-});
--- js/slides.js
+++ js/slides.js
@@ -20,3 +20,26 @@ $(".slides").slick({
             }
         }]
     });
+
+$(".slides2").slick({
+        useTransform: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        arrows: false,
+        fade: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+        dots: false,
+        draggable: false,
+        vertical: false,
+        cssEase: 'ease-out',
+        speed: 750,
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                dots: false
+            }
+        }]
+    });
