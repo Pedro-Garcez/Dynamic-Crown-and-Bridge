6e36a4795643e26118a55a72333a39da14e50413
Pedro Garcez <pedropaulogarcez@gmail.com>
2017-02-05 01:32:24 -0500

    Added comments to slides.js. Removed hover effects on the at a glance icons for screens under 992px.

--- js/slides.js
+++ js/slides.js
@@ -1,83 +1,88 @@
- if ($(window).width() < 992) {
-     $('.slides2').on('beforeChange', function() {
-         $(this).find(".p-toggler").fadeOut(150);
-     });
-     $('.slides2').on('afterChange', function() {
-         $(this).find(".p-toggler").fadeIn(150);
-     });
- }
+// Auto display the at a glance icons paragraph and hide it when sliding away.
 
- $(".slides").slick({
-     useTransform: true,
-     autoplay: true,
-     autoplaySpeed: 5000,
-     arrows: false,
-     fade: true,
-     slidesToShow: 1,
-     slidesToScroll: 1,
-     pauseOnHover: false,
-     pauseOnFocus: false,
-     dots: false,
-     draggable: false,
-     vertical: false,
-     mobilefirst: true,
-     cssEase: 'ease',
-     speed: 2000,
-     responsive: [{
-         breakpoint: 1024,
-         settings: {
-             dots: false
-         }
-     }]
- });
+if ($(window).width() < 992) {
+    $('.slides2').on('beforeChange', function() {
+        $(this).find(".p-toggler").fadeOut(150);
 
- $(".slides2").slick({
-     useTransform: true,
-     autoplay: true,
-     autoplaySpeed: 6000,
-     arrows: true,
-     fade: false,
-     slidesToShow: 1,
-     slidesToScroll: 1,
-     pauseOnHover: true,
-     pauseOnFocus: false,
-     dots: true,
-     draggable: true,
-     vertical: false,
-     cssEase: 'ease-out',
-     speed: 750,
-     responsive: [{
-         breakpoint: 992,
-         settings: {
-             arrows: true
+    });
+    $('.slides2').on('afterChange', function() {
+        $(this).find(".p-toggler").fadeIn(150);
+    });
+}
 
-         }
-     }]
+//Slider options.
 
- });
+$(".slides").slick({
+    useTransform: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    arrows: false,
+    fade: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    pauseOnHover: false,
+    pauseOnFocus: false,
+    dots: false,
+    draggable: false,
+    vertical: false,
+    mobilefirst: true,
+    cssEase: 'ease',
+    speed: 2000,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+            dots: false
+        }
+    }]
+});
 
- $(".slides3").slick({
-     useTransform: true,
-     autoplay: true,
-     autoplaySpeed: 5000,
-     arrows: true,
-     fade: false,
-     slidesToShow: 4,
-     slidesToScroll: 1,
-     pauseOnHover: true,
-     pauseOnFocus: true,
-     dots: true,
-     draggable: true,
-     vertical: false,
-     cssEase: 'ease-out',
-     speed: 750,
-     responsive: [{
-         breakpoint: 992,
-         settings: {
-             slidesToShow: 1,
-             arrows: true
+$(".slides2").slick({
+    useTransform: true,
+    autoplay: true,
+    autoplaySpeed: 6000,
+    arrows: true,
+    fade: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    pauseOnHover: true,
+    pauseOnFocus: false,
+    dots: true,
+    draggable: true,
+    vertical: false,
+    cssEase: 'ease-out',
+    speed: 750,
+    responsive: [{
+        breakpoint: 992,
+        settings: {
+            arrows: true
 
-         }
-     }]
+        }
+    }]
 
- });
+});
+
+$(".slides3").slick({
+    useTransform: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    arrows: true,
+    fade: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    pauseOnHover: true,
+    pauseOnFocus: true,
+    dots: true,
+    draggable: true,
+    vertical: false,
+    cssEase: 'ease-out',
+    speed: 750,
+    responsive: [{
+        breakpoint: 992,
+        settings: {
+            slidesToShow: 1,
+            arrows: true
+
+        }
+    }]
+
+});
